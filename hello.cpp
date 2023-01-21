78cffdb2eb533f0b4373e7c5ef0f3e933998fb48
TanaTTario <122384343+nititornBS@users.noreply.github.com>
2023-01-22 02:08:15 +0700

    first commit

---
+++ hello.cpp
@@ -0,0 +1,116 @@
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+void mergesort(int *arr, size_t left, size_t right);
+void merge(int *arr, size_t left, size_t mid, size_t right);
+size_t binarySearch(int *arr, size_t left, size_t right, int key);
+
+int main()
+{
+    int N, i;
+    int *arr;
+    // printf("Enter the number :");
+    printf("enternumber");
+    scanf("%d", &N);
+    arr = (int *)malloc(N * sizeof(int));
+    for (i = 0; i < N; i++)
+    { 
+        scanf("%d",&arr[i]);
+    }
+
+    mergesort(arr, 0, N-1);
+
+    for (i = 0; i < N; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    free(arr);
+
+    exit(EXIT_SUCCESS);
+}
+
+void mergesort(int *arr, size_t left, size_t right)
+{               
+    if (left < right)
+    {printf("yoq");
+        size_t mid = (left + right) / 2;
+        mergesort(arr, left, mid);
+        mergesort(arr, mid+1 , right);
+        merge(arr, left, mid, right);
+    }
+}
+
+/**
+ * Merge two adjacent sorted numbers
+ * First sub-array is arr[left..mid]
+ * Second sub-array is arr[mid+1..right]
+ *
+
+ * @param arr a reference to- or a memory address of an array
+ * @param left a left index of sorting area
+ * @param mid a midpoint index of separating two sub-array
+ * @param right a right index of sorting area
+ * 
+ */
+void merge(int *arr, size_t left, size_t mid, size_t right)
+{   
+    int i;
+    // int *key=right;
+// if (left!=(right-1))
+// {
+     printf("\n%d    %d  %d ",arr [left],arr[mid],arr[right]);
+      printf("\nleft = %d mid = %d right = %d \n ",left,mid,right);
+
+   if (left = mid)
+   {
+    
+   }
+   else
+    {
+    /* code */
+   }
+   
+   
+        // binarySearch(arr,left,mid,key);
+        // binarySearch(arr,mid,right,key);
+    
+// }
+
+   
+    
+   
+   
+  
+    
+  
+    
+    
+
+    // TODO: For out-of-place merging,
+
+    // you'll need to allocate a temporary area.
+    // The number of elements can be calculated from the given indices.
+
+    // TODO: iteratively pick the numbers from both sub-arrays
+    // see merging animation on slide 8
+}
+
+/**
+ * Find an occurence of key in array
+ *
+ * @param arr a reference to- or a memory address of an sorted array
+ * @param left a left index of the array
+ * @param right a right index of the array
+ * @param key a key to be searched in for its occurence in the array
+ * @return an index to the occurence, return (right + 1) if not found
+ */
+size_t binarySearch(int *arr, size_t left, size_t right, int key)
+{
+
+    // TODO: find the midpoint
+
+    // TODO: check if the midpoint is the key
+    // TODO: if it is, return the index
+    // TODO: if it's not, do the same thing on the left or right part
+}
